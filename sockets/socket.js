bc76aae2423718b8d99f0e0be550081f9d620b22
andrespd99 <andres.epacheco99@gmail.com>
2022-05-26 16:12:13 -0400

    Clase 34

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,10 +1,22 @@
 const {
     io
 } = require('../index');
+const Band = require('../models/band');
+
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Los Mesoneros'));
+bands.addBand(new Band('Viniloversus'));
 
 // Socket messages
 io.on('connection', client => {
     console.log('Client connected...');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Client disconnected!');
